a625dd480a71995c3b60a87f1b6e58e30de4a652
Farrukh Karimov <farrukh.karimov@huawei.com>
2020-10-13 13:41:44 +0300

    Developed a distributed banking system.

---
+++ distributed-banking-system/process.h
@@ -0,0 +1,37 @@
+/**
+ * @file     ipc.h
+ * @Author   Michael Kosyakov and Evgeniy Ivanov (ifmo.distributedclass@gmail.com)
+ * @date     March, 2014
+ * @brief    A simple IPC library for programming assignments
+ *
+ * Students must not modify this file!
+ */
+
+//#ifndef __IFMO_DISTRIBUTED_CLASS_IPC__H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <fcntl.h>
+
+
+//------------------------------------------------------------------------------
+
+typedef struct{
+	pid_t pid;
+	int read;
+	int write;
+} __attribute__((packed)) Network;
+
+typedef struct{
+	int brothers_number;
+	int parent_pid;
+	int my_id;
+	int my_status;
+	pid_t my_pid;
+	Network my_network[12][2];
+} __attribute__((packed)) Process;
+
+//------------------------------------------------------------------------------
+
+//#endif // __IFMO_DISTRIBUTED_CLASS_IPC__H
+
